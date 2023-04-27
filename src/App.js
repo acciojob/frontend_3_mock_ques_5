6a59234ed01f355e873f27be9cf227d5a262bb99
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2023-04-27 10:44:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  const users = [
+    { name: "Rahul", age: 18, id: 1 },
+    { name: "Ananya", age: 21, id: 2 },
+    { name: "Tina", age: 20, id: 3 },
+    { name: "Naman", age: 19, id: 4 }
+  ];
+  return (
+    <>
+      <h3>User names</h3>
+    </>
+  );
+}
